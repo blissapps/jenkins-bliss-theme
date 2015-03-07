e28fa4b16b2d49652ceeaa7f99ce9ed354caef9b
Tiago Janela <tjanela@blissapplications.com>
2015-03-07 00:14:26 +0000

    updated to subdir

--- jenkins-theme/copy_styles.rb
+++ jenkins-theme/copy_styles.rb
@@ -1,11 +1,11 @@
 # Copy the styles from the master branch to the gh-pages branch
 `mkdir /tmp/jenkins-isotope-style`
 `cp *.css /tmp/jenkins-isotope-style`
-`git checkout gh-pages`
+`git checkout jenkins-theme`
 `git fetch origin`
-`git merge origin/gh-pages`
-`cp /tmp/jenkins-isotope-style/*.css ./`
+`git merge origin/jenkins-theme`
+`cp /tmp/jenkins-bliss-style/*.css ./`
 `git add *.css`
 `git commit -m"Updated styles"`
-`git push origin gh-pages`
-`git checkout master`
+`git push origin jenkins-theme`
+`git checkout jenkins-theme`
